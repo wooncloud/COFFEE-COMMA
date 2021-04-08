21c89485e52f76f8fce536730b2ba7711e9352dd
wooncloud <wooncloud@hotmail.co.kr>
2021-04-08 21:04:04 +0900

    menu json test

--- js/comma.js
+++ js/comma.js
@@ -1,12 +1,27 @@
-$(document).ready(function(){
-    let option = {
-        fullWidth: true,
-        indicators: true
-    }
-    $('.carousel').carousel(option);
-    $('.sidenav').sidenav();
+window.onload = function () {
+    loadJSON('../resources/menu.json',
+        function (data) { console.log(data); },
+        function (xhr) { console.error(xhr); }
+    );
+}
 
-    setInterval(function(){
-        $('.carousel').carousel('next');
-    }, 5000);
-  });
+function initMenu() {
+
+}
+
+function loadJSON(path, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                if (success)
+                    success(JSON.parse(xhr.responseText));
+            } else {
+                if (error)
+                    error(xhr);
+            }
+        }
+    };
+    xhr.open("GET", path, true);
+    xhr.send();
+}
